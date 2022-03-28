4f05262fa226773a671b16af2e2794a52482c53f
Ruxandra Grigorie <ruxi.grigorie@gmail.com>
2022-03-28 18:23:06 +0300

    GUI

---
+++ ContactsManager/app/src/main/java/com/example/contactsmanager/ContactsManagerActivity.java
@@ -0,0 +1,14 @@
+package com.example.contactsmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ContactsManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_manager);
+    }
+}
